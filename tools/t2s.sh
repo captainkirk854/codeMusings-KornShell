1357699590ee36969c650e813dd73e82c49fc340
captainkirk854 <patrickfraioli@hotmail.com>
2016-01-27 16:47:40 +0000

    refactored tab->space convertor

--- tools/t2s.sh
+++ tools/t2s.sh
@@ -1,10 +1,12 @@
 #!/bin/ksh
 #---------------------------------------------------------------------------------------------------------------------
-# Purpose : Simple, no frills File Tab to Space Convertor
+# Purpose : Simple, no frills File Tab <> Space Convertor
+# Related : expand, unexpand GNU coreutils written by David MacKenzie.
 #
 # Author      Date         Version     Comments
 # ------      ----------   -------     --------
 # Fraioli     2016-01-25       1.0     Created
+# Fraioli     2016-01-27       1.1     Some refactoring to allow multi-file processing
 #---------------------------------------------------------------------------------------------------------------------
 
 #########################
@@ -19,6 +21,75 @@ FnFileSize()
 #
  cat $file | wc -c
 }
+#-----------------------------------
+
+#-----------------------------------
+FnSubstituteTabs()
+#-----------------------------------
+{
+ sourceFile=$1
+#
+ echo -e " > $sourceFile \c"
+ targetFile=$sourceFile.$cfgTargetSuffix
+#
+ expand -t $cfgSpacesForTab $sourceFile > $targetFile
+ if [ $? -eq 0 ];then
+  size_source=`FnFileSize $sourceFile`
+  size_target=`FnFileSize $targetFile`
+  if [ $size_target -eq $size_source ] ;then
+    status=""
+    rm $targetFile > /dev/null 2>&1
+  else
+    if [ $size_target -gt $size_source ] ;then
+      status="[tab(x1)=space(x$cfgSpacesForTab) $size_source -> $size_target byte(s)]"
+      cp -p $targetFile $sourceFile > /dev/null 2>&1
+      if [ $? -eq 0 ];then
+        rm $targetFile >/dev/null 2>&1
+      fi
+    fi
+  fi
+  echo $status
+ else
+  return 1
+ fi
+#
+ return 0
+}
+#-----------------------------------
+
+#-----------------------------------
+FnSubstituteSpaces()
+#-----------------------------------
+{
+ sourceFile=$1
+#
+ echo -e " > $sourceFile \c"
+ targetFile=$sourceFile.$cfgTargetSuffix
+#
+ unexpand -t $cfgSpacesForTab $sourceFile > $targetFile
+ if [ $? -eq 0 ];then
+  size_source=`FnFileSize $sourceFile`
+  size_target=`FnFileSize $targetFile`
+  if [ $size_target -eq $size_source ] ;then
+    status=""
+    rm $targetFile > /dev/null 2>&1
+  else
+    if [ $size_target -lt $size_source ] ;then
+      status="[space(x$cfgSpacesForTab)=tab(x1) $size_source -> $size_target byte(s)]"
+      cp -p $targetFile $sourceFile > /dev/null 2>&1
+      if [ $? -eq 0 ];then
+        rm $targetFile >/dev/null 2>&1
+      fi
+    fi
+  fi
+  echo $status
+ else
+  return 1
+ fi
+#
+ return 0
+}
+#-----------------------------------
 
 
 
@@ -30,27 +101,61 @@ FnFileSize()
 #-----------------------------------
 # Configuration ..
 #-----------------------------------
-spacesForTab=4
-targetSuffix="t2s"
+cfgSpacesForTab=4
+cfgTargetSuffix="t2s"
+cfgOptionTabToSpace="-ts"
+cfgOptionSpaceToTab="-st"
 
+#-----------------------------------
+# Initialise ..
+#-----------------------------------
+product=`basename $0 .sh`
+
+# Check dependent tools ..
+which expand >/dev/null 2>&1
+if [ $? -eq 0 ];then
+  which unexpand >/dev/null 2>&1
+  if [ $? -ne 0 ];then
+    echo "UTILITY NOT FOUND: unexpand"
+    echo " ... check \$path"
+    echo " ... aborting"
+    exit 1
+  fi
+else
+ echo "UTILITY NOT FOUND: expand"
+ echo " ... check \$path"
+ echo " ... aborting"
+ exit 1
+fi
 
 #-----------------------------
 # handle input arguments ..
 #-----------------------------
-if [ $# -eq 1 ];then
-  sourceFile=$1
-  targetFile=$sourceFile.$targetSuffix
-  expand -t $spacesForTab $sourceFile > $targetFile
-  if [ $? -eq 0 ];then
-   size_original=`FnFileSize $sourceFile`
-   size_target=`FnFileSize $targetFile`
-   if [ $size_target -eq $size_original ] ;then
-     echo "No Tab replacement(s) necessary."
-     rm $targetFile
-   else
-     echo "Tab replacement(s) necessary. See --> $targetFile"
-   fi
-  else
-   echo "! UNFORESEEN PROBLEM ENCOUNTERED !" 
-  fi
+if [ $# -gt 1 ];then
+  mode=$1
+  FileList=`echo $* | cut -f2- -d" "`
+  cmdFileList="ls $FileList"
+
+  echo ""
+  for file in `eval $cmdFileList`
+  do
+    if [ -f $file ];then
+      if [ $mode = $cfgOptionTabToSpace ];then
+        FnSubstituteTabs $file
+      elif [ $mode = $cfgOptionSpaceToTab ];then
+        FnSubstituteSpaces $file
+      fi
+      if [ $? -ne 0 ];then
+        echo "  *** something went wrong -- aborting ***" 
+        echo ""
+       exit 1
+      fi
+    fi
+  done
+  echo ""
+else
+ echo ""
+ echo "Usage:"
+ echo "  $product [$cfgOptionTabToSpace|$cfgOptionSpaceToTab] *.[filesuffix]"
+ echo ""
 fi
